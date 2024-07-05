1112ef7454ee6a0f052e94a2a1aeb42f6cc651fa
caio-rondow <62180440+caio-rondow@users.noreply.github.com>
2024-07-05 01:01:32 -0300

    Update slider.py

--- minpy/slider.py
+++ minpy/slider.py
@@ -5,12 +5,12 @@ class Slider:
     def __init__(self, size : int, extras : int, radix : int) -> None:
         
         self.WIN_BITS   : int = math.ceil(math.log2(size))
-        self.EXT_BITS   : int = math.ceil(radix / 2) + extras
+        self.EXT_BITS   : int = math.ceil(radix / 2) * extras
         self.WORD_BITS  : int = 2 * self.WIN_BITS + self.EXT_BITS
         self.WIN_MASK   : int = size - 1
         self.SRC_MASK   : int = radix - 1
         self.SLIDE_RATE : int = math.ceil(radix / 2)
-
+        
     def slide(self, word : int, col : int) -> int:
         return (word >> (self.WORD_BITS - col * self.SLIDE_RATE - self.WIN_BITS)) & self.WIN_MASK
 
